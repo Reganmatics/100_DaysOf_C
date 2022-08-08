c2e5ba5c748ea43037eae692937cd549b2b96dc5
Reganmatics <devreganmatics@gmail.com>
2022-08-08 23:56:26 +0100

    display without printf or puts

---
+++ Day3/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * AUTHOR: Reganmatics
+ * main - function entry point
+ * description: this function displays the specified string
+ * Return: always (0) as an indicator of success
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	return (0);
+}
