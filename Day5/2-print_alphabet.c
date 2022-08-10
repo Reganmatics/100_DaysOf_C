d07e3ce8ae3fedd9e1d56e3429aaf609b3c8257f
Reganmatics <devreganmatics@gmail.com>
2022-08-10 23:25:18 +0100

    add task 2 initial commit for day 5

---
+++ Day5/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - function entry point
+ * Description: displays the lowercase alphabets {a..z} 
+ * using only two `putchar` functions
+ * Return: always (0) as an indicator of success
+ */
+
+
+int main(void)
+{
+	char ch = 'a';
+	char z_ch = 'z';
+
+
+	while (ch != z_ch)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
